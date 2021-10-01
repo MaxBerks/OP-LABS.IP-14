480ca416075480237719957c5ba9b9631b56ba16
MaxBerks <89969030+MaxBerks@users.noreply.github.com>
2021-10-01 12:05:35 +0300

    Add files via upload

---
+++ oplab1(C++).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b, c;
+    cout << "Enter number a:\n";
+    cin >> a;
+    cout << "Enter number b:\n";
+    cin >> b;
+    c = (a - b) / 2;
+    float PLa = b * c;
+    float PLb = b * b;
+    float PL = 4 * PLa + PLb;
+    cout << "Your answer is:" << PL << endl;
+}
---
+++ oplab1(PY).py
@@ -0,0 +1,9 @@
+print('Enter number a')
+a = float(input())
+print('Enter number b')
+b = float(input())
+c = (float(a) - float(b)) / 2
+PLa = float(b) * float(c)
+PLb = float(b) * float(b)
+PL = 4 * PLa + PLb
+print("Your answer is:",PL)
