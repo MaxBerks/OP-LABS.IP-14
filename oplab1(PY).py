ef547f7c54f9e4f2429628753f16fc87cbf0093f
MaxBerks <89969030+MaxBerks@users.noreply.github.com>
2021-10-14 13:39:23 +0300

    Delete oplab1(PY).py

--- oplab1(PY).py
+++
@@ -1,9 +0,0 @@
-print('Enter number a')
-a = float(input())
-print('Enter number b')
-b = float(input())
-c = (float(a) - float(b)) / 2
-PLa = float(b) * float(c)
-PLb = float(b) * float(b)
-PL = 4 * PLa + PLb
-print("Your answer is:",PL)
